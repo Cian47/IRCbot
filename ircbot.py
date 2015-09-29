9e6f5efff0acfecc4a1b3b3336a67e83637da998
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2015-09-29 13:32:33 +0200

    splitting of msgs

--- ircbot.py
+++ ircbot.py
@@ -5,6 +5,21 @@ import thread
 import time
 
 class IRCbot(object):
+                    
+    def __init__(self, args):
+        self.args=args
+        self.server="irc.underworld.no"
+        self.port=6667
+        self.nick="Cyborg47"
+        self.channel="#birc"
+        #self.ircPassword
+        #self.SSL=
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect()
+        self.recv()
+        #thread.start_new_thread(self.recvThread,())
+        print "started..?"
+        
         
     def connect(self):
         self.sock.connect((self.server, self.port))
@@ -28,32 +43,35 @@ class IRCbot(object):
     def pong(self):
         self.sock.send("PONG :pingis\n")  
             
-    def recvThread(self):
-        print "..."
+    def recv(self):
         while True:
-            print "...2"
             recv = self.sock.recv(2048).strip('\n\r')
-            print "===\nRECV\n===\n",recv
-            if recv.find("PING :") != -1: # if the server pings us then we've got to respond!
-                print "pingpong"
-                self.pong()
-            
+            if len(recv)!=0:
+                print "===\nRECV\n===\n",recv
+                
+                if recv.split(":")[0]=="PING ":
+                    print "pingpong"
+                    self.pong()
+                else if len(recv)<300: #tmp for now, there are bigger packets at session start...
+                    try:
+                        _, msg_header, msg_payload = recv.split(":",2)
+                        identification, msg_type, msg_receiver = msg_header.strip(" ").split(" ")
+                        sender=identification.split("!~")
+                        
+                        print recv.split(":",2)
+                        print msg_header.split(" ") 
+                        print "sender: ",sender
+                    except IndexError:
+                        if self.args.verbose:
+                            print "IndexError"
+                        pass
+                    except ValueError: # no normal channel/private message
+                        if self.args.verbose:
+                            print "ValueError"
+                        pass
+            else:
+                print "disconnect ???"
+                input(" ... ")
                 
                 
-                
-                
-    def __init__(self, args):
-        self.args=args
-        self.server="irc.underworld.no"
-        self.port=6667
-        self.nick="Cyborg47"
-        self.channel="#birc"
-        #self.ircPassword
-        #self.SSL=
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.connect()
-        self.recvThread()
-        #thread.start_new_thread(self.recvThread,())
-        print "started..?"
-        
-        
+    

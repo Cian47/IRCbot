b5e9bb6a71e961f00ac053bf85b6818ad4ee8c55
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2015-09-30 20:50:18 +0200

    added --hello for testing

--- main.py
+++ main.py
@@ -9,5 +9,6 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", action="store_true", help="be verbose")
+    parser.add_argument("--hello", action="store_true", help="testmod")
     args=parser.parse_args()
     main(args)

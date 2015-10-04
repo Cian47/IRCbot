01f77e9a5d3b3f73dd2bf7b7092952b3b482500f
Kevin Freeman <kevin.freeman@stud.uni-goettingen.de>
2015-10-04 17:05:10 +0200

    keeping track of channels, greeting ppl and timescheduler and thresholds for mods added

--- ircbot.py
+++ ircbot.py
@@ -11,13 +11,14 @@ import sys  # read input for ownSend
 TOPIC="332"
 USERLIST="353"
 EOFNAMES="366"
+GREETONJOIN=True
 
 class IRCbot(object):         
     def __init__(self, args):
         self.args=args
         self.server="irc.underworld.no"
         self.port=6667
-        self.nick="Botler"
+        self.nick="Abb0t"
         self.channels={}
         #self.ircPassword
         #self.SSL=
@@ -41,13 +42,16 @@ class IRCbot(object):
         if self.args.verbose:
             print "Joining Channel %s"%chan
         self.channels[chan]=Channel(chan)
-        print "CREATED CHANNEL OBJECT"
-        print self.channels
+        #print "CREATED CHANNEL OBJECT"
+        #print self.channels
         self.sock.send("JOIN "+ chan +"\n")
         
     def leaveChannel(self, chan):
-        print "Joining Channel %s"%chan
-        self.sock.send("LEAVE "+ chan +"\n")
+        if self.args.verbose:
+            print "Leaving Channel %s"%chan
+        if chan in self.channels: del self.channels[chan]
+        self.sock.send("PRIVMSG "+ chan + " :Gotta go mates, have a good time!\n")
+        self.sock.send("PART "+ chan +"\n")
         
     def message(self, to, msg):
         self.sock.send("PRIVMSG "+ to +" :"+msg+"\n")
@@ -61,7 +65,7 @@ class IRCbot(object):
             
     def recv(self):
         running_mods=[]
-        mods=["pong"]
+        mods=["pong","mensa"]
         for m in mods:
             exec "import mods.%s"%m
             ## module starting here ##
@@ -83,8 +87,27 @@ class IRCbot(object):
                     self.pong()
                 elif len(recv.split("\n"))==1:  # single msg = single line ;)
                     print recv
+                    
+                    #we parse it, might need it #TODO make method for this
+                    try:
+                        _, msg_header, msg_payload = recv.split(":",2)
+                        identification, msg_type, msg_receiver = msg_header.strip(" ").split(" ")
+                        sender=identification.split("!")
+                        
+                    except IndexError:
+                        print "IndexError"
+                        pass
+                    except ValueError: # no normal channel/private message
+                        print "ValueError"
+                        pass
+                    
+                    #but we give the whole recv to mods, they may parse on their own                        
                     for mod in running_mods:
-                        mod.cmd(recv)
+                        #print "%d - %d > %d\n%d > %d"%(time.time(),mod.lastcmd,mod.resttime,time.time() - mod.lastcmd,mod.resttime)
+                        if time.time() - mod.lastcmd > mod.resttime:
+                            mod.cmd(recv)
+                        else:
+                            mod.queue_out.put("PRIVMSG "+ msg_receiver +" : - I need a total rest of %d seconds - \n"%mod.resttime)
                 else:
                     for l in recv.split("\n"):
                         l=l.strip("\r")
@@ -103,12 +126,24 @@ class IRCbot(object):
                                         print "ERROR. TOPIC OF %s BUT NO CHANNELOBJECT!"%(content[4])
                                     pass
                                 elif msg_type==EOFNAMES:
+                                    if GREETONJOIN:
+                                        user_list=[]
+                                        for u in self.channels[content[3]].users:
+                                            if u!=self.nick and u[1:]!=self.nick:
+                                                user_list.append(u)
+                                        if len(user_list)>0:
+                                            self.sock.send("PRIVMSG "+ content[3] +" :Hello %s\n"%(', '.join(user_list)))
                                     pass
                                 elif msg_type==USERLIST:
                                     try:
                                         self.channels[content[4]].users=cols[2].strip(" ").split(" ")
                                     except KeyError:
                                         print "ERROR. USERLIST OF %s BUT NO CHANNELOBJECT!"%(content[4])
+                                    pass
+                                elif msg_type=="432":  # erroneous nickname;)
+                                    print "error in nickname, choose another one!"
+                                    input(" ... ") # TODO exit
+                                elif msg_type=="MODE": # modechange e.g. oncreate of channel
                                     pass
                                 elif self.args.verbose:
                                     print "UNKOWN MSG_TYPE %s @ %s"%(msg_type,cols)
@@ -136,6 +171,14 @@ class IRCbot(object):
             print "ENTER MSG NOW:"
             data = sys.stdin.readlines()
             for d in data:
-                self.sock.send(d)
+                d=d.strip("\n")
+                parameters=filter(None,d.split(" "))
+                print parameters
+                if parameters[0]=="JOIN":
+                    self.joinChannel(parameters[1])
+                elif parameters[0]=="PART":
+                    self.leaveChannel(parameters[1])
+                else:
+                    self.sock.send(d+"\n")
         
     
